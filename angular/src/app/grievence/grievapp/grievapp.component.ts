cf45d74537ffb65c9d04d385c140fe00a6fbb13d
Nitesh Bisht <niteshbisht98@gmail.com>
2017-09-24 03:24:03 +0530

    Grievance Changes

--- angular/src/app/grievence/grievapp/grievapp.component.ts
+++ angular/src/app/grievence/grievapp/grievapp.component.ts
@@ -8,14 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class GrievappComponent implements OnInit {
   public states:any;
   public selectedState:any;
+  public selectedRegion:any;
+  public selectedDept:any;
+  public grieveText:any;
 
   constructor() {
     this.states={};
     this.selectedState="";
     this.states.names=[{'name':'A'},{'name':'B'},{'name':'C'},{'name':'D'}];
+    this.states.region=[{'name':'A'},{'name':'B'},{'name':'C'},{'name':'D'}];
+    this.states.dept=[{'name':'A'},{'name':'B'},{'name':'C'},{'name':'D'}];
    }
 
   ngOnInit() {
+  }
+
+  formSubmit(params:any){
+    console.log(params);
   }
 
 }
